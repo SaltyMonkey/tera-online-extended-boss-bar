3da70008d8bac68791ac3a73dc6a7268ab699be5
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2020-09-24 19:49:54 +0300

    api update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-module.exports = function ExtendedBossBar(m) {
-   m.installGPK("S1UI_GageBoss.gpk");
-}
+exports.ClientMod = class {
+   install(installer) {
+     installer.gpk("S1UI_Message.gpk");
+   }
+};
